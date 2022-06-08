efcc1076419e5b6ed3a6d79a45ba019435115b12
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-06-08 17:46:26 +0200

    ejercicios 3 JavaScript

---
+++ ej3.js
@@ -0,0 +1,15 @@
+let string = 'Mi Variable';
+let num = 1;
+let bool = true;
+let nu = null;
+let unde;
+let sym = Symbol('Something');
+let obj = { nombre: 'Juan Carlos'}
+let arr = [1, "Pescador", false]
+let scoGlobal = '';
+let fun = function power(){
+    let scoLocal = '';
+}
+
+console.log(nu)
+console.log(typeof arr);

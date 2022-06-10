acd4c76dc879e4b15f61f7d15b2fcdf2693670d1
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-06-10 09:53:53 +0200

    ejercicio 15 JavaScript

--- ej15.js
+++ ej15.js
@@ -1,21 +1,26 @@
-const d2 = document.getElementsByClassName("md1");
-const d3 = document.getElementsByClassName("md2");
-const sc = document.getElementsByClassName("sc");
+let cont = 0;
 
 function acierto(){
-    let cont = 0;
-    if(cont = 0){
-        document.getElementById("md0").style.display = "none";
+    let d1 = document.getElementById("md0");
+    let d2 = document.getElementById("md1");
+    let d3 = document.getElementById("md2");
+    let d4 = document.getElementById("md3");
+    let sc = document.getElementById("sc");
+
+    if( cont === 0){
+        d1.style.display = "none";
         d2.style.display = "block";
         cont++;
-        sc++;
-    }else if(cont = 1){
+        sc.textContent = cont;
+    }else if( cont === 1){
         d2.style.display = "none";
         d3.style.display = "block";
         cont++;
-        sc++;
-    }else if(cont = 2){
+        sc.textContent = cont;
+    }else if( cont === 2){
         d3.style.display = "none";
-        sc++;
+        cont++;
+        sc.textContent = cont;
+        d4.style.display = "block";
     }
 }
